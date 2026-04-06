d28bf54908e03870c17af33ed433e4322b87c022
thebmnbloc <bioralak46@gmail.com>
2026-04-06 09:22:08 +0200

    ad bior_name fn

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,17 @@ fn main() {
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{}", s); // this will print `hello, world!`
+
+    bior_name();
+}
+
+
+
+
+
+fn bior_name() -> String {
+   let mut name = String::from("Bior"); // creates a String from a string literal
+   name.push_str(" is awesome!"); // appends a string literal to the String
+   println!("{}", name); // prints the string
+   name
 }
